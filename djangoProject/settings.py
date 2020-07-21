c8db02706244745196db44100ca9c26276a3a726
Arthur W <apwinck@gmail.com>
2020-07-21 11:18:26 -0300

    moar fixes :(

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+(1*xja)vrqlo1um$z17xx3-8gb$6ce(%pp737)(m*n4v067&m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'assets'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
