e80871844da03759125cc3612c317de8cfc88763
Arthur W <apwinck@gmail.com>
2020-07-21 10:38:45 -0300

    fixes pt3

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -140,10 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'assets/static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'assets'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 MEDIAFILES_ROOT = os.path.join(BASE_DIR,'media')
