730dec2229eb00c2e098da38fdea1706cdf2e90d
Arthur W <apwinck@gmail.com>
2020-07-22 19:53:06 -0300

    Set Debug value

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG_VALUE']
+
+DEBUG = (os.environ['DEBUG_VALUE'] == 'True')
 
 ALLOWED_HOSTS = []
 
